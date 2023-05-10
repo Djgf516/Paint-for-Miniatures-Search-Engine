e2b988bf9a0162db880848c5d5c5c2a2b085d500
Demetrio Gutierrez Finley <demetriogutierrezfinley@gmail.com>
2023-05-10 16:59:35 -0400

    Working on getting the replace CSV function to work

--- src/main/java/application/SearchBarDriver.java
+++ src/main/java/application/SearchBarDriver.java
@@ -16,50 +16,49 @@ public class SearchBarDriver {
     public String choice = "";
 
 
-
     //------------------------------------------------------------
     // Start Search bar
     //------------------------------------------------------------
     private List<InfoPaint> infoPaints = readPaintFromCSV("Test.csv");
 
-    private int paintNameCounter =0;
-    private int paintBrandCounter =0;
-    private int paintHexCounter =0;
+    private int paintNameCounter = 0;
+    private int paintBrandCounter = 0;
+    private int paintHexCounter = 0;
 
     public void run() {
         // let's print all the paint read from CSV file
 
-            while (true) {
-                //Welcome the User to the program
-                UserInput.displayHomeScreen();
+        while (true) {
+            //Welcome the User to the program
+            UserInput.displayHomeScreen();
 
 //                for (InfoPaint b : infoPaints) {
 //                   System.out.println(b);}
 
-                //Get the user search option
-                choice = UserInput.getSearchOption();
+            //Get the user search option
+            choice = UserInput.getSearchOption();
 
-           System.out.println( nameSearch(choice));
+            System.out.println(nameSearch(choice));
 
-            }
         }
+    }
 
-    public  InfoPaint nameSearch (String option) {
+    public InfoPaint nameSearch(String option) {
 
         InfoPaint search = null;
 
-        for (InfoPaint currentPaint : infoPaints){
+        for (InfoPaint currentPaint : infoPaints) {
 
             //Search the Paint name return 1 value
 
-            if (option.trim().equalsIgnoreCase(currentPaint.getPaintName())){
+            if (option.trim().equalsIgnoreCase(currentPaint.getPaintName())) {
 
-                search= currentPaint;
+                search = currentPaint;
             }
 
-            if (option.trim().equalsIgnoreCase(currentPaint.getBrandName())){
+            if (option.trim().equalsIgnoreCase(currentPaint.getBrandName())) {
 
-                search= currentPaint;
+                search = currentPaint;
             }
 
         }
@@ -70,7 +69,7 @@ public class SearchBarDriver {
     }
 
 
-    }
+}
 
 
 
