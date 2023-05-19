f29aa8cd9488a0f1fa74d2b0b4f00fe37a32b922
Demetrio Gutierrez Finley <demetriogutierrezfinley@gmail.com>
2023-05-19 14:26:44 -0400

    Updating SQL files

--- sql/TestRequests.sql
+++ sql/TestRequests.sql
@@ -34,21 +34,51 @@ TRIM (hex_color_code)
 --command " "\\copy public.vallejo_game (name, brand, hex_color_code, match_value) FROM 'C:/Users/Student/DOWNLO~1/VALLEJ~1.CSV' DELIMITER ',' CSV QUOTE '\"' ESCAPE '''';""
 
 SELECT*
-FROM paint
+FROM paint;
 WHERE name ILIKE '%beiGeRed%'
 LIMIT 25;
+#29556d
 
+SELECT*
+FROM paint
+WHERE hex_color_code LIKE '%29556d';
 
 SELECT *
 FROM match_paint;
 
+--Search using paint name
+SELECT * 
+FROM paint 
+WHERE paint_id IN(SELECT match_paint.comparison_paint_id
+				FROM paint
+				JOIN match_paint ON match_paint.base_paint_id = paint.paint_id
+				WHERE paint.name LIKE '%Green%')
+LIMIT 25;
+
+--Search using Hex Color Code				
+SELECT * 
+FROM paint 
+WHERE paint_id IN(SELECT match_paint.comparison_paint_id
+				FROM paint
+				JOIN match_paint ON match_paint.base_paint_id = paint.paint_id
+				WHERE paint.hex_color_code LIKE '%29556d');
+
+
+--Just the matches for the paint
 SELECT match_paint.comparison_paint_id
 FROM paint
 JOIN match_paint ON match_paint.base_paint_id = paint.paint_id
-WHERE paint.name LIKE '%Stormy%';
+WHERE paint.name LIKE '%Green%';
+
 
 SELECT mp.match_value, p.name, p.brand, p.hex_color_code AS comparison_paint_name
 FROM match_paint mp
 JOIN paint p ON mp.comparison_paint_id = p.paint_id
 JOIN paint p_base ON mp.base_paint_id = p_base.paint_id
-WHERE p_base.name LIKE '%Red%';
+WHERE p_base.name ILIKE '%Red%'
+ORDER BY p.name;
+
+
+
+
+
